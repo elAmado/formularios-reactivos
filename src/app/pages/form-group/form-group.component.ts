f89a67a2ab947db90c399a0c684c65f939be3550
unknown <edwin_amado@hotmail.com>
2021-07-02 13:56:42 -0500

    excelente ;)

--- src/app/pages/form-group/form-group.component.ts
+++ src/app/pages/form-group/form-group.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from "@angular/forms";
+import { FormBuilder, Validators } from "@angular/forms";
 
 @Component({
 	selector: 'app-form-group',
@@ -7,16 +7,43 @@ import { FormGroup, FormControl } from "@angular/forms";
 	styleUrls: ['./form-group.component.scss'],
 })
 export class FormGroupComponent implements OnInit {
-	formContacto = new FormGroup({
-		primerNombre: new FormControl(''),
-		nombreDeUsuario: new FormControl(''),
-		ciudad: new FormControl(''),
-		estado: new FormControl(''),
-		codigoPostal: new FormControl('')
+
+	formContacto = this.fb.group({
+		primerNombre: ['', [Validators.min(4), Validators.max(88)]],
+		nombreDeUsuario: [''],
+		ciudad: [''],
+		estado: [''],
+		codigoPostal: [''],
 	});
 
-	constructor() {}
+	constructor(private fb: FormBuilder) {}
 
 	ngOnInit(): void {}
 
+	enviarFormContacto(): void {
+		console.log('Valor del formContacto =>  ', JSON.stringify(this.formContacto.value));
+	}
+
+	valoresPorDefecto(): void {
+		const contacto = {
+			primerNombre: 'Edwin',
+			nombreDeUsuario: "elamado",
+			ciudad: "Cutervo",
+			estado: "Perú",
+			codigoPostal: '51',
+		}
+		this.formContacto.setValue(contacto);
+	}
+
+	onPatchValue(): void {
+		this.formContacto.patchValue({ ciudad: 'Payac' });
+	}
+
+	onSetValue(): void {
+		this.formContacto.setValue({ primerNombre: 'ppp', nombreDeUsuario: "nnn", ciudad: "ccc", estado: "eee"}); //xxx
+	}
+
+	limpiarCampos(): void {
+		this.formContacto.reset();
+	}
 }
